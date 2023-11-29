9d55741a7fa73bbe4eb899ad8462ece69f2c25ba
Mimi <mimifox42@gmail.com>
2023-11-29 12:45:33 -0500

    Home Page v1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,49 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
-@vite('resources/css/app.css')
+    <meta http-equiv= "X-UA-Compatible" content="IE-edge"></meta>
+    <meta charset="utf-8"></meta>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link  rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>Header Footer Menu</title>
+    <div class="navbar">
+        <h1 class ="title"> Card Wiz Pro</h1>
+        <a class="active" href="#home">Home</a> 
+        <a href="#learnmore">Learn More</a>
+        <a href="#contact">Contact Us</a>
+        <a href="#aboutus">About Us</a>
+        <input type="text" placeholder="Search..">
+    </div> 
 </head>
 <body>
-<h1> Hello World
-</h1>
+<div class="page-wrapper"> </div>
 </body>
+    <div class="footer">
+        <div class="footer-content">
 
+        <div class="footer-section about">
+            <h1> Card Wiz Pro </h1>
+            <p> Text about Card Wiz Pro ...</p>
+        </div>
+
+        <div class="footer-section link">
+            <h2> Links </h2>
+            <ul>
+            <a href="#home"><li>Home</li></a> 
+            <a href="#learnmore"><li>Learn More</li></a>
+            <a href="#contact"><li>Contact Us</li></a>
+            <a href="#aboutus"><li>About Us</li></a>
+            </ul>
+        </div>
+
+        <div class="footer-section contact-form">
+            <h2> Support </h2>
+            <a href="#"><i class="fa fa-comments" aria-hidden="true"> Live Chat</i> </a>
+            </br>
+            <a href ="#"><i class="fa fa-phone" aria-hidden="true"> 111-222-3334</i> </a>
+            
+        </div>  
+    </div>
+</div>
 </html>
+

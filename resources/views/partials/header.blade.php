4f2907ffe6e1c3bd6619e99f74844cf56dd01554
Mimi <mimifox42@gmail.com>
2023-12-01 13:02:23 -0500

    Created header and footer partials

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv= "X-UA-Compatible" content="IE-edge"></meta>
+    <meta charset="utf-8"></meta>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link  rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>Header Footer Menu</title>
+    <div class="navbar">
+        <h1 class ="title"> Card Wiz Pro</h1>
+        <a class="active" href="#home">Home</a> 
+        <a href="#learnmore">Learn More</a>
+        <a href="#contact">Contact Us</a>
+        <a href="#aboutus">About Us</a>
+        <input type="text" placeholder="Search..">
+    </div> 
+</head>
+<body>
+<div class="page-wrapper"> </div>
+</body>
+</html>

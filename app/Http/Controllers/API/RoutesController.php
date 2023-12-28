e5d89a9373472609ffa3431501c9fcb907b80317
Solomon Byer <sol@payquad.com>
2023-12-28 18:46:10 -0500

    saving

--- app/Http/Controllers/API/RoutesController.php
+++ app/Http/Controllers/API/RoutesController.php
@@ -1,10 +1,39 @@
 <?php
 
 namespace App\Http\Controllers\API;
+use App\Http\Controllers\API\merchantsController;
+use App\Models\ApiKey;
 
 use Illuminate\Http\Request;
 
 class RoutesController extends Controller
 {
-    //
+     function handlePaymentRequest($apiKey, $live, $paymentId = null)
+    {
+        // Check if API key exists in the database
+        $api = ApiKey::where('api_key', $apiKey)->first();
+    
+        if ($api) {
+            // Set endpoint based on the "live" parameter
+            $endpoint = $live ? 'https://finix.live-payments-api.com' : 'https://finix.sandbox-payments-api.com';
+    
+            // Call the appropriate function based on the presence of $paymentId
+            if ($paymentId) {
+                $result = merchantsController::fetchPayment(config("app.api_username"), config("app.api_password"), $paymentId, $endpoint);
+            } else {
+                $result = merchantsController::listPayments(config("app.api_username"), config("app.api_password"), config(), $endpoint, ['filter' => json_encode(['tags.api' => $apiKey])]);
+            }
+    
+            return response()->json($result);
+        } else {
+            // API key not found
+            return response()->json(['error' => 'Invalid API key'], 401);
+        }
+    }
+    function listPayments(){
+        return $this->handlePaymentRequest(request('api_key'), request('live', false));
+    }
+    function fetchPayment(){
+        return $this->handlePaymentRequest(request('api_key'), request('live', false), request('payment_id'));
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,39 +1,10 @@
 <?php
 
-use App\Http\Controllers\MerchantsController;
-use App\Models\ApiKey;
+use App\Http\Controllers\API\RoutesController;
 use Illuminate\Support\Facades\Route;
 
-// Define a function to handle common logic for both routes
-function handlePaymentRequest($apiKey, $live, $paymentId = null)
-{
-    // Check if API key exists in the database
-    $api = ApiKey::where('api_key', $apiKey)->first();
-
-    if ($api) {
-        // Set endpoint based on the "live" parameter
-        $endpoint = $live ? 'https://finix.live-payments-api.com' : 'https://finix.sandbox-payments-api.com';
-
-        // Call the appropriate function based on the presence of $paymentId
-        if ($paymentId) {
-            $result = MerchantsController::fetchPayment(config("app.api_username"), config("app.api_password"), $paymentId, $endpoint);
-        } else {
-            $result = MerchantsController::listPayments(config("app.api_username"), config("app.api_password"), config(), $endpoint, ['filter' => json_encode(['tags.api' => $apiKey])]);
-        }
-
-        return response()->json($result);
-    } else {
-        // API key not found
-        return response()->json(['error' => 'Invalid API key'], 401);
-    }
-}
-
 // Define the listPayments route
-Route::post('/listPayments', function () {
-    return handlePaymentRequest(request('api_key'), request('live', false));
-});
+Route::post('/listPayments', [RoutesController::class,'listPayments' ]);
 
 // Define the fetchPayment route
-Route::post('/fetchPayment', function () {
-    return handlePaymentRequest(request('api_key'), request('live', false), request('payment_id'));
-});
+Route::post('/fetchPayment', [RoutesController::class,'fetchPayment' ]);

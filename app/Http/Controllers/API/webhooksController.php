895f55da0df371c54a98c999ce544c0848d22d7d
Solomon Byer <sol@payquad.com>
2024-01-22 00:20:56 -0500

    added missing isset

--- app/Http/Controllers/API/webhooksController.php
+++ app/Http/Controllers/API/webhooksController.php
@@ -158,7 +158,7 @@ class webhooksController extends Controller
 
                 foreach ($files as $file) {
                     $modelClass = $this->getModelNamespace($file);
-                    if(class_exists($modelClass) && array_keys((array)$eventData->_embedded)[0]==$modelClass::$name){
+                    if(class_exists($modelClass) && isset($modelClass::$name) && array_keys((array)$eventData->_embedded)[0]==$modelClass::$name){
                         foreach($eventData->_embedded->{$modelClass::$name} as $item){
                             $item=(object)$item;
                         if (method_exists($modelClass, 'updateFromId')) {
